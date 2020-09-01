7e86319aa3afb357e9a3473ae7aa16b9287cbe5b
yiwatani <dbhammer17@gmail.com>
2020-09-01 17:54:59 +0900

    create save function

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -6,4 +6,18 @@ class TweetsController < ApplicationController
   def new
     @tweet = Tweet.new
   end
+
+  def create
+    Tweet.create(tweet_params)
+  end
+
+  def destroy
+    tweet = Tweet.find(params[:id])
+    tweet.destroy
+  end
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:name, :image, :text)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   root 'tweets#index'
-  resources :tweets, only: [:index, :new]
+  resources :tweets, only: [:index, :new, :create, :destroy]
 end
