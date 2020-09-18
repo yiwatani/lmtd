46b48a72623b6e7c741cf52e1eb9632a0aeffaa0
yiwatani <dbhammer17@gmail.com>
2020-09-18 16:47:23 +0900

    ページネーション実装

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,7 +2,7 @@ class CategoriesController < ApplicationController
   before_action :set_category, only: :show
 
   def show
-    @tweets = Tweet.where(category_id: params[:id]).order("created_at DESC")
+    @tweets = Tweet.where(category_id: params[:id]).order("created_at DESC").page(params[:page]).per(10)
   end
 
   private
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,7 +3,7 @@ class TweetsController < ApplicationController
   before_action :move_to_index, except: [:index, :show, :search]
   
   def index
-    @tweets = Tweet.includes(:user).order("created_at DESC")
+    @tweets = Tweet.includes(:user).order("created_at DESC").page(params[:page]).per(10)
     @categories = Category.all
   end
 
