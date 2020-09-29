36e36fc151b9323e87c2b266ebf987ca194452a3
yiwatani <dbhammer17@gmail.com>
2020-09-29 19:28:05 +0900

    Update sessions_controller.rb

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,7 +1,8 @@
 class Users::SessionsController < Devise::SessionsController
   def new_guest
-    user = User.find_or_create_by!(email: 'guest@sample.com', nickname: 'ゲスト') do |user|
+    user = User.find_or_create_by!(email: 'guest@sample.com') do |user|
       user.password = SecureRandom.urlsafe_base64
+      user.nickname = 'ゲスト'
     end
     sign_in user
     redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
