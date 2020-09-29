0e1a88d1702e3431fc71ddef0fde240067bd1237
yiwatani <dbhammer17@gmail.com>
2020-09-29 18:43:42 +0900

    ゲストログイン機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users
   root 'tweets#index'
+  devise_for :users
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#new_guest'
+  end
   resources :tweets do
     resources :comments, only: :create
     collection do
       get 'search'
     end
     member do
-      post "create_like"
+      post 'create_like'
     end
   end
   resources :users, only: :show
