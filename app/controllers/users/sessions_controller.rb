13ff09bb2f47d5c77fe49001b235ae7baf4ecb27
yiwatani <dbhammer17@gmail.com>
2020-09-29 19:14:06 +0900

    ゲストユーザーニックネーム付与

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,6 +1,6 @@
 class Users::SessionsController < Devise::SessionsController
   def new_guest
-    user = User.find_or_create_by!(email: 'guest@sample.com') do |user|
+    user = User.find_or_create_by!(email: 'guest@sample.com', nickname: 'ゲスト') do |user|
       user.password = SecureRandom.urlsafe_base64
     end
     sign_in user
